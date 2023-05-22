8a0ce47666df554941c3ffbf6d4cabd3d05a7377
Ecoder-JianZhou <ecoder-zj@outlook.com>
2023-05-22 11:55:20 -0400

    pull cybercommon and ecopadq 05/22/2023

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # EcoPAD_v2023
-A new version of EcoPAD based on new CyberCommon which puts all component to containers (Jian).
+A new version of EcoPAD based on new CyberCommon which puts all component to containers (Jian) - 5/22/2023.
+
+# Pre-work
+- pull the CyberCommon and ecopadq from the github of ou-ecolad that developed by Markus (5/22/2023).
+- change some documents in EcoPAD, such as the "ecopadq" to "ecopad"
---
+++ ecopad/setup.py
@@ -0,0 +1,19 @@
+#import ez_setup
+#ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+setup(name='ecopadq',
+      version='0.3',
+      packages= find_packages(),
+#      package_data={'ecopadq':['ecopadq/tasks/templates/*.tmpl']},
+      install_requires=[
+        'paramiko',
+#       'dockertask @ git+git://github.com/ou-ecolab/dockertask.git@test#egg=dockertask',
+#       'celery',
+#       'requests',
+#       'jinja2',
+      ],
+#      dependency_links=[
+#          'https://github.com/ouinformatics/dockertask/tarball/master#egg=dockertask-0.0',
+#      ],
+#      include_package_data=True,
+)
